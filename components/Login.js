cf43337c4012f16d52c453bc2fc57cb19c0ca9a0
Matt Smith <msmithfl@icloud.com>
2023-04-11 15:28:52 -0400

    Firebase config done

--- components/Login.js
+++ components/Login.js
@@ -19,7 +19,7 @@ export default function Login() {
         {isLoggingIn ? "Login" : "Register"}
       </h1>
       {error && (
-        <div className="w-full max-w-[30ch] text-center border-rose-400 border border-solid text-rose-400 py-2">
+        <div className="w-full max-w-[40ch] text-center border-rose-400 border border-solid text-rose-400 py-2">
           {error}
         </div>
       )}
@@ -28,18 +28,18 @@ export default function Login() {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Email Address"
-        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[30ch]"
+        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[40ch]"
       />
       <input
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         type="password"
         placeholder="Password"
-        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[30ch]"
+        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[40ch]"
       />
       <button
         onClick={submitHandler}
-        className="w-full mt-1 max-w-[30ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900"
+        className="w-full mt-1 max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900"
       >
         <h2 className="relative z-20">SUBMIT</h2>
       </button>
